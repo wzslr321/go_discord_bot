39d5b49c295fdd9b34fdc598406d19ce64619d7c
Wiktor ZAając <wiktortechwebdev@gmail.com>
2020-11-14 03:45:24 +0100

    code cleanup and comments added

--- bot.go
+++ bot.go
@@ -22,6 +22,7 @@ func init() {
 	flag.Parse()
 }
 
+//  Whole func 'main' contains basic bot configuration, responsible for start,close,status and messageListener
 func main() {
 
 	dg, err := discordgo.New("Bot " + Token)
@@ -54,11 +55,15 @@ func main() {
 
 }
 
+//  Sets bot status to '!talk'
 func ready(s *discordgo.Session, event *discordgo.Ready){
 	 s.UpdateStatus(0, "!talk")
 }
+/*
+	messageEmbed constructor with MessageEmbed type, gives a possibility to
+ 	use create var and use it  in s.ChannelMessageSendEmbed function
 
-
+ */
 func newMessageEmbed(title string ,description string ) *discordgo.MessageEmbed {
 	msg := discordgo.MessageEmbed{
 		Title:title,
@@ -67,13 +72,12 @@ func newMessageEmbed(title string ,description string ) *discordgo.MessageEmbed 
 	return &msg
 }
 
-
-var commandListsEmbed = newMessageEmbed(
-	"Here is list of my commands!",
-	"1 : Embed message creator \n 2 : Check time \n 3:  Open calculator \n Type !talk <option> to choose one",
-)
-
+//   Creates messageEmbed and send it to channel where it was invoked by user`s message
 func sendCommands(s * discordgo.Session, m *discordgo.MessageCreate) {
+	 commandListsEmbed := newMessageEmbed(
+		"Here is list of my commands!",
+		"1 : Embed message creator \n 2 : Check time \n 3:  Open calculator \n Type !talk <option> to choose one",
+	)
 	_, err := s.ChannelMessageSendEmbed(m.ChannelID,commandListsEmbed)
 	if err != nil{
 		fmt.Println("error occurred ::",err)
@@ -81,9 +85,15 @@ func sendCommands(s * discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
+//   Global vars used as validation in message listener
 var isMenuNumberTrue bool
 var selectedOption int
 
+/*
+     Function which loops for every possible option and checks if
+	 user entered correct option, sends message on channel if true
+
+ */
 func getEnteredOption(option string,s *discordgo.Session, m *discordgo.MessageCreate ) {
 
 	index := strings.Index(option,"")
@@ -107,6 +117,7 @@ func getEnteredOption(option string,s *discordgo.Session, m *discordgo.MessageCr
 	return
 }
 
+//  Invokes one of func based on what menu option user have entered
 func invokeMenuFuncs(opt int, s *discordgo.Session, m *discordgo.MessageCreate) {
 	switch opt {
 	case 1:
@@ -121,6 +132,7 @@ func invokeMenuFuncs(opt int, s *discordgo.Session, m *discordgo.MessageCreate) 
 	}
 }
 
+//  Sends instruction of how to create EmbedMessage
 func embedInstruction(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	howToUseEmbed := newMessageEmbed(
@@ -135,6 +147,8 @@ func embedInstruction(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 }
+
+//  Shows actual time in readable format
 func showTime(s *discordgo.Session, m *discordgo.MessageCreate) {
 	showTimeMsg := time.Now().Format("2006-01-02 15:04:05")
 	_, err := s.ChannelMessageSend(m.ChannelID,"Actual time is : " + showTimeMsg)
@@ -143,6 +157,8 @@ func showTime(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 }
+
+//  Sends instruction of how to use calculator, same way as it was in embedInstruction func
 func openCalculator(s *discordgo.Session, m *discordgo.MessageCreate) {
 	howToUseCalculator := newMessageEmbed(
 		"How to use calculator",
@@ -158,6 +174,12 @@ func openCalculator(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
+/*
+	Function responsible for creating embed message based on user`s entered data
+	It checks if user entered at least one sign in title and description,
+	sends created embed message to channel where was invoked
+
+ */
 func createEmbed(s *discordgo.Session, m *discordgo.MessageCreate, title string, description string) {
 	if len(title) >= 1  && len(description) >= 1 {
 		createdMsgEmbed := newMessageEmbed(
@@ -173,6 +195,13 @@ func createEmbed(s *discordgo.Session, m *discordgo.MessageCreate, title string,
 	}
 }
 
+/*
+	Prototype of calculation function without final calculation based on sign
+	It operates on floats to make it possible to have more precised results
+	Checks if result is integer by checking decimal numbers and
+	display result with different precision based on it
+
+ */
 func calcFuncProto(s *discordgo.Session, m *discordgo.MessageCreate, calcResult float64) {
 	convCalcResult := strconv.FormatFloat(calcResult,'f',4,32)
 	calcIndex := strings.Index(convCalcResult, ".")
@@ -203,7 +232,11 @@ func calcFuncProto(s *discordgo.Session, m *discordgo.MessageCreate, calcResult 
 	}
 }
 
+/*
+	4 functions below calculate result with proper calculation sign
+	and pass it as argument to calcFuncProto
 
+ */
 func addValues(s *discordgo.Session, m *discordgo.MessageCreate,firstVal float64, secondVal float64) {
 	calculationResult := firstVal + secondVal
 	calcFuncProto(s,m,calculationResult)
@@ -224,44 +257,50 @@ func divideValues(s *discordgo.Session, m *discordgo.MessageCreate,firstVal floa
 	calcFuncProto(s,m,calculationResult)
 }
 
+/*
+	calculate function checks what sign have user entered
+	It loops for every calculation option and checks
+	if it is equal to calculation sign that user have entered
+	Saves user sign properly only if there is no whitespace between sign
+	because of strings.Index being precised to one character
+	It also separates values using this index, coverts them
+	into float number and pass as argument to correct function
+
+ */
 func calculate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	calcOptions := []string{"+","-","*",":"}
-	calculateOptions := make(map[string]string)
-
-	calculateOptions["+"] = "+"
-	calculateOptions["-"] = "-"
-	calculateOptions["*"] = "*"
-	calculateOptions[":"] = ":"
 
 	msg := m.Content
 
 	for _,calcOption:= range calcOptions {
-		for _,opts := range calculateOptions {
-			if opts == calcOption {
-				firstValIndex := strings.Index(msg,"%")
-				secondValIndex := strings.Index(msg,calcOption)
-				if secondValIndex != -1 && firstValIndex != -1 {
-					firstVal := msg[firstValIndex+2:secondValIndex]
-					convFirstVal,_ :=  strconv.ParseFloat(firstVal,64)
-					secondVal := msg[secondValIndex+1:]
-					convSecondVal,_ :=  strconv.ParseFloat(secondVal,64)
-					switch opts {
-					case "+":
-						addValues(s,m,convFirstVal,convSecondVal)
-					case "-":
-						subtractValues(s,m,convFirstVal,convSecondVal)
-					case "*":
-						multiplyValues(s,m,convFirstVal,convSecondVal)
-					case ":":
-						divideValues(s,m,convFirstVal,convSecondVal)
-					}
-
-				}
+		firstValIndex := strings.Index(msg, "%")
+		secondValIndex := strings.Index(msg, calcOption)
+		if secondValIndex != -1 && firstValIndex != -1 {
+			firstVal := msg[firstValIndex+2 : secondValIndex]
+			convFirstVal, _ := strconv.ParseFloat(firstVal, 64)
+			secondVal := msg[secondValIndex+1:]
+			convSecondVal, _ := strconv.ParseFloat(secondVal, 64)
+			switch calcOption {
+			case "+":
+				addValues(s, m, convFirstVal, convSecondVal)
+			case "-":
+				subtractValues(s, m, convFirstVal, convSecondVal)
+			case "*":
+				multiplyValues(s, m, convFirstVal, convSecondVal)
+			case ":":
+				divideValues(s, m, convFirstVal, convSecondVal)
 			}
 		}
 	}
 }
 
+/*
+	Message event listener, which is invoked everytime user enters message
+	Uses 'strings' library to check if message contains prefix
+	It also checks length or message to make it possible to use menu
+	without invoking other functions
+
+ */
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Author.ID == s.State.User.ID {
